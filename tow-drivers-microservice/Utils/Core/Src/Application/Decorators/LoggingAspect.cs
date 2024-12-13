20aca2186f0f178008e5cd5fc6f07a5b0b05d0ca
joseeg.perez <joseegpc@gmail.com>
2024-12-13 00:28:35 -0400

    feat: :tada: [Tow drivers - 000] boilerplate code

---
+++ tow-drivers-microservice/Utils/Core/Src/Application/Decorators/LoggingAspect.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Core
+{
+    public class LoggingAspect<T, U>(IService<T, U> service, Logger logger) : IService<T, U>
+    {
+        private readonly IService<T, U> _service = service;
+        private readonly Logger _logger = logger;
+        async public Task<Result<U>> Execute(T data)
+        {
+            _logger.Log($"Attempting to execute {_service.GetType().Name} with data {data}");
+            var res = await _service.Execute(data);
+
+            if (res.IsError)
+            {
+                _logger.Log($"Error executing {_service.GetType().Name} with data {data}");
+            }
+            else
+            {
+                _logger.Log($"Successfully executed {_service.GetType().Name} with data {data}");
+            }
+
+            return res;
+        }
+    }
+}
---
+++ tow-drivers-microservice/Utils/Core/Src/Utils/ExceptionParser.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Core
+{
+    public static class ExceptionParser
+    {
+        public static Exception Parse(Exception e)
+        {
+            if (e is ApplicationError)
+            {
+                return new InvalidOperationException(e.Message);
+            }
+
+            return new InvalidDataException(e.Message);
+        }
+    }
+}
